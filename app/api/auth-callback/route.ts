c3c814e8754fbd43015106bcb97dc9e3ca1fdd98
nabn <nabeen.khadka@gmail.com>
2023-09-06 15:01:47 +1000

    fix: redirect to home after login

--- app/api/auth-callback/route.ts
+++ app/api/auth-callback/route.ts
@@ -1,3 +1,4 @@
+import { Routes } from "@/routes";
 import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { NextResponse, type NextRequest } from "next/server";
@@ -10,5 +11,5 @@ export async function GET(request: NextRequest) {
     await supabase.auth.exchangeCodeForSession(code);
   }
 
-  return NextResponse.redirect(requestUrl.origin);
+  return NextResponse.redirect(Routes.home);
 }
